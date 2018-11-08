c7ab8fb9c9b186882644337527b37031324f2ef8
Sam van Doorn <samvandoorn96@gmail.com>
2018-11-08 13:02:22 +0100

    Roll a ball

---
+++ UFO Game 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float Speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    private void Start()
+    { 
+        rb = GetComponent<Rigidbody2D>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+
+        rb.AddForce(movement * Speed);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 4)
+        {
+            winText.text = "You won!";
+        }
+    }
+}
